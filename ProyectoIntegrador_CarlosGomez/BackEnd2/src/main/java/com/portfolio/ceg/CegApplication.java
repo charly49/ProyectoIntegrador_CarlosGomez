c5e141400b7d0a1297b26e831de704ea9f9b6f44
charly49 <carlosg49@hotmail.com>
2022-08-09 16:10:39 -0300

    Creación de la carpeta y proyecto BackEnd2

---
+++ ProyectoIntegrador_CarlosGomez/BackEnd2/src/main/java/com/portfolio/ceg/CegApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ceg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CegApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_CarlosGomez/BackEnd2/src/test/java/com/portfolio/ceg/CegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ceg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
