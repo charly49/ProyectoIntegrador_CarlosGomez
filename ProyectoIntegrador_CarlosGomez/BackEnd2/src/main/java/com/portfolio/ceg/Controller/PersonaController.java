5cf2ad4b30a93ffd1326649cf466a2adc7bca85f
charly49 <carlosg49@hotmail.com>
2022-08-19 12:43:27 -0300

    cambios en Bakend para deploy
    Cambios en el bak para deploy en Firebase, Heroku base de datos en Clever Coud

--- ProyectoIntegrador_CarlosGomez/BackEnd2/src/main/java/com/portfolio/ceg/Controller/PersonaController.java
+++ ProyectoIntegrador_CarlosGomez/BackEnd2/src/main/java/com/portfolio/ceg/Controller/PersonaController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-prueba1.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -56,7 +56,7 @@ public class PersonaController {
         return persona;
     }
     
-    @GetMapping("personas/traer/perfil")
+    @GetMapping("/personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
     }
